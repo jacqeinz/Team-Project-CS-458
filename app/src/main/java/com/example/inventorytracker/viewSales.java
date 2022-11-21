aaff4ad2f9626abeb6ed15156ff4e261ac065474
jacqeinz <jacquicv.19@gmail.com>
2022-11-21 16:02:32 -0700

    added Dummy Data

--- app/src/main/java/com/example/inventorytracker/viewSales.java
+++ app/src/main/java/com/example/inventorytracker/viewSales.java
@@ -14,6 +14,7 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class viewSales extends AppCompatActivity {
+    //varialbes
     ListView viewSales;
     ArrayList<String> sales = new ArrayList<String>();
     ArrayAdapter arrayAdapterSale;
@@ -25,7 +26,9 @@ public class viewSales extends AppCompatActivity {
         SQLiteDatabase db = openOrCreateDatabase("inventory", Context.MODE_PRIVATE, null);
 
         viewSales = findViewById(R.id.view);
+        //get information from database
         final Cursor c = db.rawQuery("select * from sales",null);
+        //create columns
         int proid = c.getColumnIndex("proid");
         int proname = c.getColumnIndex("proname");
         int qty = c.getColumnIndex("qty");
@@ -33,22 +36,24 @@ public class viewSales extends AppCompatActivity {
         int total = c.getColumnIndex("total");
         sales.clear();
 
-
+        //get arrayadapter to display on listview
         arrayAdapterSale = new ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,sales);
         viewSales.setAdapter(arrayAdapterSale);
-
+        //make object of type products
         final  ArrayList<products> products1 = new ArrayList<products>();
 
         //loop through rows and add to arraylist
         if(c.moveToFirst())
         {
             do{
+                //get information and attach to object of type products
                 products pro = new products();
                 pro.id = c.getString(proid);
                 pro.product = c.getString(proname);
                 pro.qty= c.getString(qty);
                 pro.price = c.getString(price);
                 pro.total = c.getString(total);
+                //add to array
                 products1.add(pro);
                 sales.add(c.getString(proid) + " \t " + c.getString(proname) + " \t "  + c.getString(qty)   + " \t " + c.getString(price) + " \t " + c.getString(total));
 
